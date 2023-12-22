6e14fedb4e48b8a4481af807ca3b6a6607fc30a5
komed3 <pk97@outlook.de>
2023-12-22 12:22:09 +0100

    cli progress bar + basic profile infos

--- maintenance/update.js
+++ maintenance/update.js
@@ -9,15 +9,42 @@ const dir = __dirname + '/../api/';
 const today = ( new Date() ).toISOString().split( 'T' )[0];
 const api = 'https://www.forbes.com/forbesapi/person/rtb/0/position/true.json';
 
+var bar;
+
+const colors = require( 'ansi-colors' );
 const axios = require( 'axios' );
+const cliProgress = require( 'cli-progress' );
 const countries = require( 'i18n-iso-countries' );
 const fs = require( 'fs' );
 
+function nextStep( step, total, chunks = '', start = 0 ) {
+
+    console.log( step );
+
+    const bar = new cliProgress.SingleBar( {
+        format: ' {bar} | ' + colors.green( 'ETA: {eta}s' ) + ' | {value} of {total} ' + chunks
+    }, cliProgress.Presets.rect );
+
+    bar.start( total, start );
+
+    return bar;
+
+}
+
 async function run() {
+
+    console.log( 'Real-time billionaires' );
+    console.log( colors.yellow( 'update data as of ' + today ) );
+    console.log( '' );
 
     /**
      * create folders (if not exists)
      */
+
+    bar = nextStep(
+        '[1/5] Getting ready',
+        3, 'steps'
+    );
 
     [
         'list/rtb/',
@@ -27,6 +54,8 @@ async function run() {
         fs.mkdirSync( dir + d, { recursive: true } );
 
     } );
+
+    bar.update(1);
 
     /**
      * check update
@@ -41,6 +70,8 @@ async function run() {
 
     }
 
+    bar.update(2);
+
     /**
      * get profile list
      */
@@ -49,11 +80,22 @@ async function run() {
         ? JSON.parse( fs.readFileSync( dir + '/profile/list' ) || '{}' )
         : {};
 
+    bar.update(3);
+    bar.stop();
+
     /**
      * fetch data
      */
 
+    bar = nextStep(
+        '[2/5] Fetching real-time data',
+        1, 'files'
+    );
+
     const response = await axios.get( api );
+
+    bar.update(1);
+    bar.stop();
 
     if(
         response.data && response.data.personList &&
@@ -64,7 +106,15 @@ async function run() {
          * process profiles in list
          */
 
-        Array.from( response.data.personList.personsLists ).forEach( ( profile ) => {
+        let i = 0;
+
+        bar = nextStep(
+            '[3/5] Process profiles',
+            response.data.personList.personsLists.length || 0,
+            'profiles'
+        );
+
+        response.data.personList.personsLists.forEach( ( profile ) => {
 
             let uri = profile.uri.trim(),
                 path = dir + 'profile/' + uri + '/';
@@ -79,15 +129,74 @@ async function run() {
 
             }
 
+            let files = fs.readdirSync( path );
+
+            /**
+             * process basic data
+             */
+
             let name = ( profile.person.name || profile.personName ).trim();
 
             let country = profile.countryOfCitizenship
                 ? countries.getAlpha2Code( profile.countryOfCitizenship, 'en' )
                 : null;
 
+            let gender = profile.gender
+                ? profile.gender.toLowerCase()
+                : null;
+
             let birthDate = profile.birthDate
                 ? new Date( profile.birthDate )
                 : null;
+
+            let industries = [].concat( profile.industries || [] ).map( a => a.trim() );
+
+            let sources = ( profile.source || '' ).trim().split( ', ' ).map( a => a.trim() );
+
+            /**
+             * save basic profile infos
+             */
+
+            fs.writeFileSync(
+                path + 'info',
+                JSON.stringify( {
+                    uri: uri,
+                    name: name,
+                    birthDate: birthDate
+                        ? birthDate.toISOString().split( 'T' )[0]
+                        : null,
+                    family: !!( profile.family || false ),
+                    gender: gender,
+                    citizenship: country,
+                    residence: {
+                        city: profile.city || null,
+                        state: profile.state || null
+                    },
+                    image: profile.squareImage || null,
+                    industry: industries,
+                    source: sources
+                }, null, 2 ),
+                { flag: 'w' }
+            );
+
+            fs.writeFileSync(
+                path + 'bio',
+                JSON.stringify( {
+                    bio: [].concat( profile.bios || [] ).map( a => a.trim() ),
+                    about: [].concat( profile.abouts || [] ).map( a => a.trim() )
+                }, null, 2 ),
+                { flag: 'w' }
+            );
+
+            /**
+             * financial assets
+             */
+
+            fs.writeFileSync(
+                path + 'assets',
+                JSON.stringify( [].concat( profile.financialAssets || [] ), null, 2 ),
+                { flag: 'w' }
+            );
 
             /**
              * add profile to list
@@ -99,7 +208,11 @@ async function run() {
                 update: today
             };
 
+            bar.update( ++i );
+
         } );
+
+        bar.stop();
 
     }
 
