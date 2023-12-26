abc3a6d45a2f14fa317006c938b249a7a54a020b
komed3 <pk97@outlook.de>
2023-12-26 19:07:30 +0100

    first tests for rename.js

--- maintenance/rename.js
+++ maintenance/rename.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-const dir = __dirname + '/../api/profile';
+const dir = __dirname + '/../api/profile/';
 
 const colors = require( 'ansi-colors' );
 const fs = require( 'fs' );
@@ -15,6 +15,9 @@ console.log( '' );
 
 if( process.argv.length == 4 ) {
 
+    let from = process.argv[2],
+        to = process.argv[3];
+
     let index = fs.existsSync( dir + '_index' )
         ? JSON.parse( fs.readFileSync( dir + '_index' ) )
         : {};
@@ -23,9 +26,19 @@ if( process.argv.length == 4 ) {
         ? JSON.parse( fs.readFileSync( dir + '_alias' ) )
         : {};
 
+    if( !( from in index ) ) {
+
+        console.log( colors.yellow( 'ERR: [' + from + '] does not exists' ) );
+
+    } else if( to in index ) {
+
+        console.log( colors.yellow( 'ERR: [' + to + '] allready exists' ) );
+
+    }
+
 } else {
 
-    console.log( colors.red( 'ERR: wrong arguments' ) );
+    console.log( colors.yellow( 'ERR: wrong arguments' ) );
     console.log( 'use "node rename.js [FROM] [TO]"' );
 
 }
