b334d689107e19583142b88f7d865f94b78b8b95
komed3 <pk97@outlook.de>
2023-12-27 11:00:36 +0100

    server.js first steps

--- server.js
+++ server.js
@@ -7,3 +7,45 @@
  */
 
 'use strict';
+
+const fs = require( 'fs' );
+
+/**
+ * express framework
+ */
+
+const express = require( 'express' );
+
+const app = express();
+
+/**
+ * static resources
+ */
+
+app.use( '/css', express.static( __dirname + '/public/styles' ) );
+app.use( '/js', express.static( __dirname + '/public/scripts' ) );
+app.use( '/res', express.static( __dirname + '/public/resources' ) );
+
+/**
+ * pug template engine
+ */
+
+const pug = require( 'pug' );
+
+/**
+ * routing
+ */
+
+const routes = require( './config/routes' );
+
+routes.forEach( ( route ) => {
+
+    //
+
+} );
+
+/**
+ * start web server
+ */
+
+const server = app.listen( 3000 );
