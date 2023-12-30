a450cda488e297f3e00c2d93792e95269c1c2094
komed3 <pk97@outlook.de>
2023-12-30 09:40:16 +0100

    create stats templates

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 module.exports = [
     [ '/profile/:uri/?', 'profile' ],
-    [ '/stats/?', 'stats' ]
+    [ '/stats/?', 'stats' ],
+    [ '/country/:single/?', 'stats-single', 'country' ],
+    [ '/country/?', 'stats-list', 'country' ],
+    [ '/industry/:single/?', 'stats-single', 'industry' ],
+    [ '/industry/?', 'stats-list', 'industry' ]
 ];
