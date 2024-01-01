8522709cd248c991dbd67e7922012fac20263bdc
komed3 <pk97@outlook.de>
2024-01-01 13:25:00 +0100

    create annual report

--- maintenance/annual.js
+++ maintenance/annual.js
@@ -7,13 +7,14 @@
 
 const api = require( './../api/endpoint' );
 const colors = require( 'ansi-colors' );
+const fs = require( 'fs' );
 const logging = require( './_logging' );
 
 const profiles = Object.keys( api.index );
 
 const year = process.argv.includes( '--year' )
-    ? process.argv[ process.argv.indexOf( '--year' ) + 1 ] || ( new Date() ).getFullYear()
-    : ( new Date() ).getFullYear();
+    ? process.argv[ process.argv.indexOf( '--year' ) + 1 ] || ( new Date() ).getFullYear() - 1
+    : ( new Date() ).getFullYear() - 1;
 
 /**
  * create annual report
@@ -37,10 +38,70 @@ async function run() {
 
         let path = '/profile/' + uri + '/';
 
-        let annual = api.getJSONFile( path + 'annual' ),
-            history = api.getCSVFile( path + 'history' );
+        let history = api.getCSVFile( path + 'history' ).filter( r => r[0].includes( year ) ),
+            dayCount = history.length;
 
-        //
+        if( dayCount ) {
+
+            let annual = api.getJSONFile( path + 'annual' );
+
+            let first = history[0], last = history[ dayCount - 1 ];
+
+            /**
+             * create annual report
+             */
+
+            let rank = history.map( r => r[1] ),
+                ntw = history.map( r => r[2] );
+
+            let report = {
+                rank: {
+                    value: last[1] || null,
+                    first: first[1] || null,
+                    diff: parseInt( last[1] - first[1] ),
+                    average: parseInt( rank.reduce(
+                        ( a, c ) => a + c, 0
+                    ) / dayCount ),
+                    max: Math.min( ...rank ) || null,
+                    min: Math.max( ...rank ) || null,
+                    range: null
+                },
+                networth: {
+                    value: last[2],
+                    first: first[2] || null,
+                    diff: Number( parseFloat( last[2] - first[2] ).toFixed(3) ),
+                    average: Number( parseFloat( ntw.reduce(
+                        ( a, c ) => a + c, 0
+                    ) / dayCount ).toFixed(3) ),
+                    max: Number( parseFloat( Math.max( ...ntw ) ).toFixed(3) ),
+                    min: Number( parseFloat( Math.min( ...ntw ) ).toFixed(3) ),
+                    range: 0
+                }
+            };
+
+            report.networth.range = Number( parseFloat( report.networth.max - report.networth.min ).toFixed(3) );
+
+            if( report.rank.max && report.rank.min ) {
+
+                report.rank.range = parseInt( report.rank.min - report.rank.max );
+
+            }
+
+            annual[ year ] = report;
+
+            /**
+             * save annual report
+             */
+
+            fs.writeFileSync(
+                __dirname + '/../api' + path + 'annual',
+                JSON.stringify( Object.keys( annual ).sort().reduce( ( a, b ) => ( {
+                    ...a, [ b ]: annual[ b ]
+                } ), {} ), null, 2 ),
+                { flag: 'w' }
+            );
+
+        }
 
         logging.update();
 
