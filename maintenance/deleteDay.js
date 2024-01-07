f237d936281173328f08395c30761e606e125d4c
komed3 <pk97@outlook.de>
2024-01-07 17:36:36 +0100

    Update deleteDay.js

--- maintenance/deleteDay.js
+++ maintenance/deleteDay.js
@@ -15,6 +15,36 @@ const date = process.argv.includes( '--date' )
     : null;
 
 /**
+ * delete file
+ * @param {String} file path to file
+ */
+const deleteFile = ( file ) => {
+
+    if( fs.existsSync( file ) ) {
+
+        fs.unlinkSync( file );
+
+    }
+
+};
+
+/**
+ * delete date from CSV file
+ * @param {String} file path to file
+ * @param {Int} field date field
+ */
+const deleteRow = ( file, field = 0 ) => {
+
+    api.saveCSVFile(
+        file,
+        api.getCSVFile( file ).filter(
+            r => r[ field ] != date
+        )
+    );
+
+};
+
+/**
  * delete day
  */
 async function run() {
@@ -24,32 +54,103 @@ async function run() {
     console.log( '' );
 
     /**
+     * delete date from real-time list & movers
+     */
+
+    logging.next(
+        '[1/3] delete date from real-time list & movers',
+        6, 'steps'
+    );
+
+    deleteRow( '/availableDays' );
+
+    logging.update();
+
+    deleteFile( __dirname + '/../api/list/rtb/' + date );
+
+    logging.update();
+
+    deleteFile( __dirname + '/../api/movers/value/winner/' + date );
+
+    logging.update();
+
+    deleteFile( __dirname + '/../api/movers/value/loser/' + date );
+
+    logging.update();
+
+    deleteFile( __dirname + '/../api/movers/pct/winner/' + date );
+
+    logging.update();
+
+    deleteFile( __dirname + '/../api/movers/pct/loser/' + date );
+
+    logging.update();
+
+    /**
      * delete date from profiles
      */
 
     let profiles = Object.keys( api.index );
 
     logging.next(
-        '[1/3] delete ' + colors.yellow( date ) + ' from profiles',
+        '[2/3] delete date from profiles',
         profiles.length, 'profiles'
     );
 
     profiles.forEach( ( uri ) => {
 
-        let path = '/profile/' + uri + '/history';
-
-        api.saveCSVFile(
-            path,
-            api.getCSVFile( path ).filter(
-                r => r[0] != date
-            )
-        );
+        deleteRow( '/profile/' + uri + '/history' );
 
         logging.update();
 
     } );
 
+    /**
+     * delete date from stats
+     */
+
+    logging.next(
+        '[3/3] delete date from stats',
+        3, 'steps'
+    );
+
+    Object.keys( api.indexes ).forEach( ( type ) => {
+
+        let index = Object.keys( api.indexes[ type ] );
+
+        logging.addTotal( index.length );
+
+        index.forEach( ( key ) => {
+
+            deleteRow( '/stats/' + type + '/' + key );
+
+            logging.update();
+
+        } );
+
+    } );
+
+    deleteRow( '/stats/count' );
+
+    logging.update();
+
+    deleteRow( '/stats/woman' );
+
+    logging.update();
+
+    deleteRow( '/stats/total' );
+
+    logging.update();
     logging.finish();
+
+    /**
+     * recommendation
+     */
+
+    console.log( '' );
+    console.log( 'recommended to run:' );
+    console.log( colors.yellow( 'node annual.js --year ' + date.split( '-' )[0] ) );
+    console.log( '' );
 
 };
 
