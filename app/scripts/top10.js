185bdc32687560291e9193d10e9e4ba39463f171
komed3 <pk97@outlook.de>
2024-01-12 16:20:14 +0100

    first steps to load top 10 month data

--- app/scripts/top10.js
+++ app/scripts/top10.js
@@ -1,9 +1,83 @@
+/**
+ * search for container after DOM content has loaded
+ * add profiles to grid and load first month
+ */
 document.addEventListener( 'DOMContentLoaded', () => {
 
-    document.querySelectorAll( '.rtb-top10-container' ).forEach( ( container ) => {
+    let container;
+    let data;
 
-        let data = JSON.parse( window.atob( container.getAttribute( 'top10-data' ) ) ),
-            grid = container.querySelector( '.rtb-top10-grid' );
+    /**
+     * get month by index
+     * @param {Int} idx list index
+     */
+    const getTop10 = ( idx = 0 ) => {
+
+        let month = Object.keys( data.list )[ idx ] || null;
+
+        if( month ) {
+
+            let date = new Date( month + '-01' ),
+                list = data.list[ month ],
+                profiles = {};
+
+            /**
+             * build new list data
+             */
+
+            list.forEach( ( p ) => {
+
+                profiles[ p.uri ] = parseFloat( p.networth );
+
+            } );
+
+            let max = Math.max( ...Object.values( profiles ) ),
+                min = Math.min( ...Object.values( profiles ) ) * 0.9,
+                range = max - min;
+
+            /**
+             * update list
+             */
+
+            container.querySelectorAll( '.rtb-top10-profile' ).forEach( ( p ) => {
+
+                let uri = p.getAttribute( 'top10-profile' ),
+                    pct = '0%', networth = '$0';
+
+                if( uri in profiles ) {
+
+                    pct = Math.max( 5, ( profiles[ uri ] - min ) / range * 100 ) + '%';
+
+                    networth = '$' + Number( ( profiles[ uri ] / 1000 ).toFixed( 2 ) ) + 'B';
+
+                }
+
+                p.querySelector( '.rtb-top10-profile-column .col' ).style.height = pct;
+                p.querySelector( '.rtb-top10-profile-column .col b' ).innerHTML = networth;
+
+            } );
+
+            /**
+             * update month
+             */
+
+            container.querySelector( '.rtb-top10-month' ).innerHTML =
+                monthNames[ date.getMonth() ] + ' ' +
+                date.getFullYear();
+
+        }
+
+    };
+
+    document.querySelectorAll( '.rtb-top10-container' ).forEach( ( c ) => {
+
+        container = c;
+
+        data = JSON.parse(
+            window.atob(
+                container.getAttribute( 'top10-data' )
+            )
+        );
 
         /**
          * add profiles
@@ -31,9 +105,15 @@ document.addEventListener( 'DOMContentLoaded', () => {
                 '<span>' + utf8( profile.source.join( ', ' ) ) + '</span>' +
             '</div>';
 
-            grid.appendChild( el );
+            container.querySelector( '.rtb-top10-grid' ).appendChild( el );
 
         }
+
+        /**
+         * load first month
+         */
+
+        getTop10();
 
     } );
 
